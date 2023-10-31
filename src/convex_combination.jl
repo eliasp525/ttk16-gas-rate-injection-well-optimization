5b7c73af0169c75a75b04e8185bbdbaa46f41880
Elias Prytz <asdero63@gmail.com>
2023-10-31 11:32:13 +0100

    Seems to be working for several Qinjmax

--- src/convex_combination.jl
+++ src/convex_combination.jl
@@ -50,88 +50,117 @@ endpointno = accumulate(+, num_points);
 variable_ranges = [endpointno[w]-num_points[w]+1:endpointno[w] for w in 1:N];
 
 
-### Create model object
-model = Model(GLPK.Optimizer);
+QINJ_MAX_RANGE = 500:5:530; #300:5:5000;
 
-### Define variables
+all_objective_values = Array{Float64}(undef, length(QINJ_MAX_RANGE));
+all_qinjes = Array{Vector{Float64}}(undef, length(QINJ_MAX_RANGE))
+Qinj_maxes = collect(QINJ_MAX_RANGE);
 
-# lambdas for all wells
-@variable(model, lambdas[i = 1:nrow(datapoints_df)] >= 0);
+for optnum in 1:length(QINJ_MAX_RANGE)
+    Qinj_max = Qinj_maxes[optnum];
 
-# binary z_i for all wells
-# z_i = 1 -> x ∈ [x_{i}, x_{i+1}]
-@variable(model, z[i = 1:nrow(datapoints_df)], Bin);
+    ### Create model object
+    model = Model(GLPK.Optimizer);
 
-# binary y_w for each well
-# y_w = 1 -> production
-# y_w = 0 -> no production 
-@variable(model, y[i = 1:N], Bin);
+    ### Define variables
 
-### Define objective
-@objective(model, Max, sum([dot(lambdas[variable_ranges[w]], datapoints_df.qoil[variable_ranges[w]]) for w in Wells]));
+    # lambdas for all wells
+    @variable(model, lambdas[i = 1:nrow(datapoints_df)] >= 0);
 
-### Define constraints
-for w in Wells
-    ## constraints for convex combination ##
-    
-    @constraint(model, sum(lambdas[variable_ranges[w]]) == 1); # sum of lambdas in range for one well must be 1
+    # binary z_i for all wells
+    # z_i = 1 -> x ∈ [x_{i}, x_{i+1}]
+    @variable(model, z[i = 1:nrow(datapoints_df)], Bin);
 
-    # sum of z_i in range for one well must equal 1
-    @constraint(model, sum(z[variable_ranges[w]]) == 1);
-    
-    # IMPORTANT:
-    # Remember that both λ and z variable start with index 1
+    # binary y_w for each well
+    # y_w = 1 -> production
+    # y_w = 0 -> no production 
+    @variable(model, y[i = 1:N], Bin);
 
-    # λ_1 <= z_1 for all wells
-    first_index_current_well = collect(variable_ranges[w])[1];
-    @constraint(model, lambdas[first_index_current_well] <= z[first_index_current_well]);
+    ### Define objective
+    @objective(model, Max, sum([dot(lambdas[variable_ranges[w]], datapoints_df.qoil[variable_ranges[w]]) for w in Wells]));
 
-    # λ_p <= z_{p-1}
-    current_well_indices = collect(variable_ranges[w]);
-    @constraint(model, lambdas[current_well_indices[end]] <= z[current_well_indices[end-1]]);
-    
-    # λ_i <= z_{i-1} + z_i
-    for i in current_well_indices[2:end-1]
-        @constraint(model, lambdas[i] <= z[i-1] + z[i]);
+    ### Define constraints
+    for w in Wells
+        ## constraints for convex combination ##
+        
+        @constraint(model, sum(lambdas[variable_ranges[w]]) == 1); # sum of lambdas in range for one well must be 1
+
+        # sum of z_i in range for one well must equal 1
+        @constraint(model, sum(z[variable_ranges[w]]) == 1);
+        
+        # IMPORTANT:
+        # Remember that both λ and z variable start with index 1
+
+        # λ_1 <= z_1 for all wells
+        first_index_current_well = collect(variable_ranges[w])[1];
+        @constraint(model, lambdas[first_index_current_well] <= z[first_index_current_well]);
+
+        # λ_p <= z_{p-1}
+        current_well_indices = collect(variable_ranges[w]);
+        @constraint(model, lambdas[current_well_indices[end]] <= z[current_well_indices[end-1]]);
+        
+        # λ_i <= z_{i-1} + z_i
+        for i in current_well_indices[2:end-1]
+            @constraint(model, lambdas[i] <= z[i-1] + z[i]);
+        end
+
+        ## constraints from original problem ##
+
+        # oil output is less than max and 0 if well is shut off (y_w = 0)
+        # \hat{q_w^o} <= q^{max, w}_o * y_w
+        # (2f in assignment)
+        Qoilmax = maximum(datapoints_df.qoil[variable_ranges[w]]);
+        @constraint(model, dot(lambdas[variable_ranges[w]], datapoints_df.qoil[variable_ranges[w]]) <= Qoilmax*y[w]);
+        
+        # qinj within bounds if well is on, if not (y_w = 0) then qinj = 0
+        # (2g in assignment)
+        @constraint(model, injectionbounds_df.low[w]*y[w] <= dot(lambdas[variable_ranges[w]], datapoints_df.qinj[variable_ranges[w]]));
+        @constraint(model, dot(lambdas[variable_ranges[w]], datapoints_df.qinj[variable_ranges[w]]) <= injectionbounds_df.high[w]*y[w]);
     end
 
-    ## constraints from original problem ##
+    ## other higher level constraints from the original problem ##
 
-    # oil output is less than max and 0 if well is shut off (y_w = 0)
-    # \hat{q_w^o} <= q^{max, w}_o * y_w
-    # (2f in assignment)
-    Qoilmax = maximum(datapoints_df.qoil[variable_ranges[w]]);
-    @constraint(model, dot(lambdas[variable_ranges[w]], datapoints_df.qoil[variable_ranges[w]]) <= Qoilmax*y[w]);
-    
-    # qinj within bounds if well is on, if not (y_w = 0) then qinj = 0
-    # (2g in assignment)
-    @constraint(model, injectionbounds_df.low[w]*y[w] <= dot(lambdas[variable_ranges[w]], datapoints_df.qinj[variable_ranges[w]]));
-    @constraint(model, dot(lambdas[variable_ranges[w]], datapoints_df.qinj[variable_ranges[w]]) <= injectionbounds_df.high[w]*y[w]);
+    # sum of qinj for all wells less than Qinj_max
+    # (2b in assignment)
+    @constraint(model, sum([dot(lambdas[var_range], datapoints_df.qinj[var_range]) for var_range in variable_ranges]) <= Qinj_max);
 
+    # sum of q_w and qoil for all wells <= Qliq_max.
+    # has been simplified from formulas
+    # (2c in assignment)  
+    @constraint(model, sum([dot(lambdas[variable_ranges[w]], datapoints_df.qoil[variable_ranges[w]])*(1/(1-wcut_df.wcut[w])) for w in 1:N]) <= Qliq_max);
+
+    # sum of qinj and q_g for all wells <= Qgas_max. 
+    # (2d in assignment)
+    @constraint(model, sum([ dot(lambdas[variable_ranges[w]], datapoints_df.qinj[variable_ranges[w]]) + gor_df.gor[w]*dot(lambdas[variable_ranges[w]], datapoints_df.qoil[variable_ranges[w]]) for w in 1:N]) <= Qgas_max);
+
+
+    optimize!(model);
+
+    for w in Wells
+        @show value.(lambdas[variable_ranges[w]]);
+    end
+
+    # total oil production
+    all_objective_values[optnum] = objective_value(model);
+
+    # injection rate for each well
+    qinjes = Vector{Float64}(undef, N);
+
+    for w in Wells
+        qinjes[w] = dot(value.(lambdas[variable_ranges[w]]), datapoints_df.qinj[variable_ranges[w]]);
+    end
+
+    all_qinjes[optnum] = qinjes;
+
+    # @show qinjes;
+
+    #@show value.(z);
+    #@show value.(y);
+    # @show objective_value(model);
+
+    # @show termination_status(model);
 
 end
 
-## other higher level constraints from the original problem ##
-
-# sum of qinj for all wells less than Qinj_max
-# (2b in assignment)
-@constraint(model, sum([dot(lambdas[var_range], datapoints_df.qinj[var_range]) for var_range in variable_ranges]) <= Qinj_max);
-
-# sum of q_w and qoil for all wells <= Qliq_max.
-# has been simplified from formulas
-# (2c in assignment)  
-@constraint(model, sum([dot(lambdas[variable_ranges[w]], datapoints_df.qoil[variable_ranges[w]])*(1/(1-wcut_df.wcut[w])) for w in 1:N]) <= Qliq_max);
-
-# sum of qinj and q_g for all wells <= Qgas_max. 
-# (2d in assignment)
-@constraint(model, sum([ dot(lambdas[variable_ranges[w]], datapoints_df.qinj[variable_ranges[w]]) + gor_df.gor[w]*dot(lambdas[variable_ranges[w]], datapoints_df.qoil[variable_ranges[w]]) for w in 1:N]) <= Qgas_max);
-
-
-optimize!(model);
-
-@show value.(lambdas);
-#@show value.(z);
-@show value.(y);
-@show objective_value(model);
-
-@show termination_status(model);
+@show all_qinjes
+@show all_objective_values
